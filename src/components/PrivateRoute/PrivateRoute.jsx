5481e303ecb8e6250d0a8816246ec303a17df85c
Dmytro Bulhakov <dmytro.bulhakov@plus4u.net>
2025-08-21 00:26:36 +0200

    Header rename

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStore } from "../../hooks/useAuthStore";
-import Header from "../header/Header";
+import Header from "../Header/Header";
 
 function PrivateRoute() {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
